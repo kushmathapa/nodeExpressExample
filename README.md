821697cccfe6d59597e0032536529e200983997b
kushmathapa <kushma@awecode.com>
2023-11-19 16:47:38 +0545

    feat: set up initial scaffold project

--- README.md
+++ README.md
@@ -1,1 +1,86 @@
-# coding-project-template
+# expressjs-template-project
+
+##Install dependencies:
+
+```console
+$ npm install
+```
+
+Start the server:
+
+```console
+$ npm start
+```
+
+### Usage
+
+- Run npm start:dev to start the application.
+- Connect to the API using Postman on port 5000.
+
+User Registration
+
+```console
+curl --location --request POST 'http://localhost:5000/register' \
+--header 'Content-Type: application/json' \
+--header 'Cookie: connect.sid=s%3AljDmXKbstAT0PWNljQF0AVz7nzt5342O.04TymrShtIC5B56RCqXDWlquV0vqVqvhtZRND9bDat8' \
+--data-raw '{
+    "username": "{username}",
+    "password": "{password}"
+}'
+```
+
+User login
+
+```console
+curl --location --request POST 'http://localhost:5000/login' \
+--header 'Content-Type: application/json' \
+--header 'Cookie: connect.sid=s%3AljDmXKbstAT0PWNljQF0AVz7nzt5342O.04TymrShtIC5B56RCqXDWlquV0vqVqvhtZRND9bDat8' \
+--data-raw '{
+    "username": "{username}",
+    "password": "{password}"
+}'
+```
+
+Get Friends
+
+```console
+curl --location --request GET 'http://localhost:5000/friends' \
+--header 'Cookie: connect.sid=s%3AljDmXKbstAT0PWNljQF0AVz7nzt5342O.04TymrShtIC5B56RCqXDWlquV0vqVqvhtZRND9bDat8'
+```
+
+Get Friends by Email Id
+
+```console
+curl --location --request GET 'http://localhost:5000/friends/andysmith@gamil.com'
+```
+
+Add Friends:
+
+```console
+curl --location --request POST 'http://localhost:5000/friends' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "email": "andysmith@gamil.com",
+    "firstName": "Andy",
+    "lastName": "Smith",
+    "DOB": "1/1/1987"
+}'
+```
+
+Update Friend
+
+```console
+curl --location --request PUT 'http://localhost:5000/friends/andysmith@gmil.com' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "firstName": "Andy",
+    "lastName": "Smith",
+    "DOB": "1/1/1987"
+}'
+```
+
+Delete Friend
+
+```console
+curl --location --request DELETE 'http://localhost:5000/friends/andysmith@gamil.com'
+```
--- router/friends.js
+++ router/friends.js
@@ -1,47 +1,75 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
 let friends = {
-    "johnsmith@gamil.com": {"firstName": "John","lastName": "Doe","DOB":"22-12-1990"},
-    "annasmith@gamil.com":{"firstName": "Anna","lastName": "smith","DOB":"02-07-1983"},
-    "peterjones@gamil.com":{"firstName": "Peter","lastName": "Jones","DOB":"21-03-1989"}
+  "johnsmith@gamil.com": {
+    firstName: "John",
+    lastName: "Doe",
+    DOB: "22-12-1990",
+  },
+  "annasmith@gamil.com": {
+    firstName: "Anna",
+    lastName: "smith",
+    DOB: "02-07-1983",
+  },
+  "peterjones@gamil.com": {
+    firstName: "Peter",
+    lastName: "Jones",
+    DOB: "21-03-1989",
+  },
 };
 
-
 // GET request: Retrieve all friends
-router.get("/",(req,res)=>{
-
-  // Update the code here
-
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+router.get("/", (req, res) => {
+  res.send(JSON.stringify(friends, null, 4));
 });
 
 // GET by specific ID request: Retrieve a single friend with email ID
-router.get("/:email",(req,res)=>{
-  // Update the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+router.get("/:email", (req, res) => {
+  const email = req.params.email;
+  friend = friends[email];
+  if (friend) res.send(friend);
+  else res.send("User does not exist");
 });
-
 
 // POST request: Add a new friend
-router.post("/",(req,res)=>{
-  // Update the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+router.post("/", (req, res) => {
+  if (req.body.email) {
+    friends[req.body.email] = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      DOB: req.body.DOB,
+    };
+  }
+  res.send("The user " + req.body.firstName + " has been added!");
 });
-
 
 // PUT request: Update the details of a friend with email id
 router.put("/:email", (req, res) => {
-  // Update the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  const email = req.params.email;
+  if (email != null) {
+    let friendToUpdate = friends[email];
+    if (friendToUpdate) {
+      let dob = req.body.DOB;
+      let firstName = req.body.firstName;
+      let lastName = req.body.lastName;
+      if (dob) friendToUpdate["DOB"] = dob;
+      if (firstName) friendToUpdate["firstName"] = firstName;
+      if (lastName) friendToUpdate["lastName"] = lastName;
+      res.send("The user " + req.body.firstName + " has been updated");
+    } else res.send("User does not exist");
+  } else res.send("Email id is null");
 });
-
 
 // DELETE request: Delete a friend by email id
 router.delete("/:email", (req, res) => {
-  // Update the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  const emailToDelete = req.params.email;
+  let friendToDelete = friends[emailToDelete];
+  if (friendToDelete) {
+    delete friends[emailToDelete];
+    res.send("User with " + emailToDelete + " has been deleted");
+  } else res.send("User with " + emailToDelete + " does not exist");
 });
 
-module.exports=router;
+module.exports = router;
